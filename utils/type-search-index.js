2848fc1ec46b1603f04676cd736a8669256aa8d7
Rahil khan <65817230+ErrorxCode@users.noreply.github.com>
2021-10-16 13:25:32 +0530

    pubg utils docs

---
+++ utils/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.pubg.utils","l":"PubgUtils.Extra"},{"p":"com.pubg.utils","l":"FileUtils"},{"p":"com.pubg.utils","l":"OneTimeDialogInterface"},{"p":"com.pubg.utils","l":"OneTimeEventInterface"},{"p":"com.pubg.utils","l":"PubgUtils"}]
